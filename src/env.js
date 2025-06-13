02bb377849b122f5c8b1d285be093f6667bd3c9e
Ivan <mayhemk57@gmail.com>
2025-06-13 16:50:41 -0600

    init

---
+++ src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? '/mypage/' : ''
+export const useSockets = false
+export const domain = 'dev-41sfkm5puotb44zl.us.auth0.com'
+export const clientId = '76eAydb08kjvivarkJ8eHFTO0XtM2fMp'
+export const audience = 'https://www.IvansProjects.com'
