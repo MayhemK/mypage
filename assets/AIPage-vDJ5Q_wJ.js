7c29f85ad06c2542f469672dbc84c8de36d47bf6
Ivan <mayhemk57@gmail.com>
2025-07-20 21:57:03 -0600

    Updates

---
+++ assets/AIPage-vDJ5Q_wJ.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-D9mzvvx9.js";const t={};function r(c,_){return" AI Page test "}const s=e(t,[["render",r]]);export{s as default};
---
+++ assets/AboutPage-C2eC4MHN.js
@@ -0,0 +1,1 @@
+import{_ as t,a as o,b as s,o as a}from"./index-D9mzvvx9.js";const n={},r={class:"about"};function c(_,e){return a(),o("div",r,e[0]||(e[0]=[s("h1",null,"This is the about page",-1)]))}const l=t(n,[["render",c]]);export{l as default};
