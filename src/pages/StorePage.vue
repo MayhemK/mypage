26ffaa5ab9ce67ca153b0639b2eb2a440ad56628
MayhemK <mayhemk57@gmail.com>
2026-02-02 21:03:13 -0700

    feat: Add StorePage component with product display and basic cart functionality.

--- src/pages/StorePage.vue
+++ src/pages/StorePage.vue
@@ -1,36 +1,43 @@
 <script setup>
+import { ref } from 'vue';
 
-
-
-var items = [
+const items = [
   {
     Name: "A",
-    Price: 1,
+    Price: "$10.99",
     Description: "stuff",
     Image: "https://images.unsplash.com/photo-1467139840664-96b244a66825?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     Name: "B",
-    Price: 1,
+    Price: "$14.99",
     Description: "More Stuff",
     Image: "https://images.unsplash.com/photo-1609041958269-fc44467d925d?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   }, {
     Name: "C",
-    Price: 1,
+    Price: "$19.99",
     Description: "even more stuff",
     Image: "https://images.unsplash.com/photo-1474666488182-66ec723476c6?q=80&w=1174&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     Name: "D",
-    Price: 1,
+    Price: "$24.99",
     Description: "even even more stuff",
     Image: "https://images.unsplash.com/photo-1674884283579-d6e496502be8?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   }
 ]
 
+const cart = ref([])
 
+function addToCart(item) {
+  console.log(item);
+  cart.value.push(item);
+}
 
-
+function removeFromCart(item) {
+  console.log(item);
+  cart.value.pop(item);
+}
 </script>
 
 
@@ -46,7 +53,7 @@ var items = [
   </section>
   <section class="container mt-4">
     <div class="row">
-      <div class="col-12">
+      <div class="col-9">
         <div class="container">
           <div class="row">
             <div v-for="item in items" :key="item.Name" class="col-3 text-center my-2">
@@ -57,13 +64,36 @@ var items = [
                     <div class="card-title fw-bold">{{ item.Name }}</div>
                     <hr>
                     <div class="card-text">{{ item.Description }}</div>
+                    <div class="card-text">{{ item.Price }}</div>
+                    <button class="btn btn-primary mdi mdi-cart-plus" @click="addToCart(item)"> Add
+                      to Cart</button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-
+      </div>
+      <div class="col-3">
+        <div class="rounded-5 bg-dark text-light text-center fs-2 align-content-center" style="height: 100px;">
+          Cart
+        </div>
+        <div class="card text-center">
+          <div v-if="cart.length === 0">
+            <div class="card-body">
+              <div class="card-text">Cart is empty</div>
+            </div>
+          </div>
+          <div v-else v-for="item in cart" :key="item.Name" class="card-body">
+            {{ item.Name }} {{ item.Price }} X 45,000
+            <hr>
+            <button class="btn btn-danger mdi mdi-cart-plus" @click="removeFromCart(item)"> Remove from Cart</button>
+          </div>
+          <div class="card-footer">
+            <div>Total: $0.00</div>
+          </div>
+        </div>
+        <button class="btn btn-primary">Checkout</button>
       </div>
     </div>
   </section>
